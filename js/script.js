75ab8f5102b95a69565996973bba7294e86390b6
Noah Milvourne <nkm48@cabrini.edu>
2019-12-08 19:26:18 -0500

    hi-fi

---
+++ js/script.js
@@ -0,0 +1,58 @@
+$.scrollIt({
+  upKey: 38,             // key code to navigate to the next section
+  downKey: 40,           // key code to navigate to the previous section
+  easing: 'linear',      // the easing function for animation
+  scrollTime: 600,       // how long (in ms) the animation takes
+  activeClass: 'active', // class given to the active nav element
+  onPageChange: null,    // function(pageIndex) that is called when page is changed
+  topOffset: 0           // offste (in px) for fixed top navigation
+});
+
+
+var myElement = document.querySelector("header");
+var headroom = new Headroom(myElement);
+headroom.init();
+
+
+$(".rslides").responsiveSlides({
+  auto: true,             // Boolean: Animate automatically, true or false
+  speed: 500,            // Integer: Speed of the transition, in milliseconds
+  timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
+  pager: false,           // Boolean: Show pager, true or false
+  nav: false,             // Boolean: Show navigation, true or false
+  random: false,          // Boolean: Randomize the order of the slides, true or false
+  pause: false,           // Boolean: Pause on hover, true or false
+  pauseControls: true,    // Boolean: Pause when hovering controls, true or false
+  prevText: "Previous",   // String: Text for the "previous" button
+  nextText: "Next",       // String: Text for the "next" button
+  maxwidth: "",           // Integer: Max-width of the slideshow, in pixels
+  navContainer: "",       // Selector: Where controls should be appended to, default is after the 'ul'
+  manualControls: "",     // Selector: Declare custom pager navigation
+  namespace: "rslides",   // String: Change the default namespace used
+  before: function(){},   // Function: Before callback
+  after: function(){}     // Function: After callback
+});
+
+
+
+
+$(document).ready(function() {
+			  //To switch directions up/down and left/right just place a "-" in front of the top/left attribute    
+			  //Full Caption Sliding (Hidden to Visible)
+			  $('.boxgrid.captionfull').hover(function() {
+			    $(".cover", this).stop().animate({
+			      top: '160px'
+			    }, {
+			      queue: false,
+			      duration: 160
+			    });
+			  }, function() {
+			    $(".cover", this).stop().animate({
+			      top: '260px'
+			    }, {
+			      queue: false,
+			      duration: 160
+			    });
+			  });
+
+			});
